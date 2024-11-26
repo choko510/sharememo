0f35dc9b97b794c903c3d9e41d6b219ea86cbe98
choko510 <135209805+choko510@users.noreply.github.com>
2024-11-27 00:45:49 +0900

    AIによる添削機能の追加

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from fastapi.responses import FileResponse
 from typing import Dict, Set, List
 import json
 import uuid
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_APIKEY")) 
 
 app = FastAPI()
 
@@ -104,6 +111,37 @@ async def websocket_endpoint(websocket: WebSocket, memo_id: str):
                         user_id
                     )
             
+            elif message["type"] == "rewrite":
+                if memo_id in memos:
+                    memo = memos[memo_id]["content"]
+                    await manager.broadcast(json.dumps({
+                            "type": "rewrite",
+                            "status": "stop",
+                            "name":user_id
+                        }), memo_id, user_id)
+
+                    model = genai.GenerativeModel("gemini-1.5-flash")
+
+                    input_text = f'''
+                    #命令
+                    - 文章を校正して下さい。
+                    - 誤字脱字なども修正して下さい。
+                    #制約条件
+                    - 元の文章の原型を保ったまま文章を変更して下さい。
+                    - 改行の位置関係などを変えないで下さい。
+                    - 出力形式は必ず添削した文章のみにして下さい。
+                    #修正文章
+                    {memo}
+                    '''
+                    
+                    response = model.generate_content(input_text)
+                    print(response.text)
+                    await manager.broadcast(json.dumps({
+                        "type": "rewrite",
+                        "status": "success",
+                        "content":response.text
+                    }), memo_id, None)
+                    
     except WebSocketDisconnect:
         manager.disconnect(memo_id, user_id)
         if memo_id in memos and "cursors" in memos[memo_id]:
